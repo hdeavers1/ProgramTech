0f61c4531defb8d72c532a5e175333ed76a92541
Hayden Deavers <98769753+hdeavers1@users.noreply.github.com>
2023-09-22 20:02:43 -0400

    lab2 complete

---
+++ Lab 2/summary.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your Major is <?php echo $_POST["major"]; ?><br>
+Your Favorite Web Language is <?php echo $_POST["fav_language"]; ?><br>
+The Development Enviroment used is <?php echo $_POST["vscode"]; ?><br>
+
+</body>
+</html>
